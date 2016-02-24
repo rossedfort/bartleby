5d30c6e5176fc2b249ccd3f1a0f518c11db3e0be
rossedfort <rossedfort@gmail.com>
2016-02-24 12:48:58 -0700

    compentize - make textarea disappear on save

--- app/controllers/notes/note.js
+++ app/controllers/notes/note.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  editing: false,
+
   actions: {
     saveNote() {
       this.get('model').save();
+      Ember.$('.rendered').toggleClass('half');
+      Ember.$('.markdown').toggle();
     }
   }
 });
