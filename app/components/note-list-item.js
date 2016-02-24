3c9c4d906dc34c331c6986366414b7c3621ea111
rossedfort <rossedfort@gmail.com>
2016-02-24 14:53:54 -0700

    fix some styling - delete functional

--- app/components/note-list-item.js
+++ app/components/note-list-item.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  truncatedNoteName: Ember.computed('note.id', function(){
+    return this.get('note.id').length > 15 ? this.get('note.id').slice(0, 15) + '...' : this.get('note.id');
+  }),
+
+  actions: {
+    delete(note) {
+      note.deleteRecord();
+      note.save();
+    }
+  }
 });
