68d38850376881b789ae1661fa130f5203c5fb5f
rossedfort <rossedfort@gmail.com>
2016-02-24 11:33:51 -0700

    made char and word count a component

--- app/controllers/notes/note.js
+++ app/controllers/notes/note.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  charCount: Ember.computed('content', function(){
-    return this.get('model.content').split(" ").join("").length;
-  }),
-
-  wordCount: Ember.computed('content', function(){
-    return this.get('model.content').split(" ").length;
-  }),
-
   actions: {
     saveNote() {
       this.get('model').save();
