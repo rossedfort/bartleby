f4e1fd19b2902d670418bb75eec694d5cc1096cf
Robbie Lane <lane.robbie@gmail.com>
2016-02-27 09:22:14 -0700

    add dates

--- app/controllers/notes/note.js
+++ app/controllers/notes/note.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
+const moment = require('moment');
 
 export default Ember.Controller.extend({
+  formattedDate: Ember.computed('model.birthTime', function() {
+    let date = new Date(this.get('model.birthTime'));
+    return moment(date).format('LL');
+  }),
+
+
   actions: {
     saveNote() {
       this.get('model').save();
